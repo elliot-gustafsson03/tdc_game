4f1cfb844faa9356b752a336606da7a2685bf6ed
Elliot <elliot.gustafsson03@gmail.com>
2024-04-19 15:11:42 +0200

    Should be working now. Just very ugly

--- server.js
+++ server.js
@@ -23,10 +23,85 @@ app.get("/admin", async (req, res) => {
 })
 
 app.post("/join", (req, res) => {
-    const newUser = req.body.name
-    console.log(newUser)
-
+    const newPlayer = req.body.name
+    addPlayer(newPlayer)
+    
     res.sendStatus(200)
 })
 
+app.post("/newRound", (req, res) => {
+    newRound()
+    res.sendStatus(200)
+})
+
+app.get("/players", (req, res) => {
+    res.send(JSON.stringify(players))
+})
+
+app.get("/getInfo", (req, res) => {
+    const name = req.query.name
+    res.send(JSON.stringify(getInfo(name)))
+})
+
 app.listen(port)
+
+
+// GAME LOGIC
+
+
+let players = []
+let lostPlayers = []
+let round = -1
+
+function addPlayer(name) {
+    if (!players.includes(name)) {
+        players.push(name)
+    }
+}
+
+function newRound() {
+    round++
+    
+    lostPlayers = []
+    lostPlayers.push(randomPlayer())
+    
+    let otherRandom = randomPlayer()
+    while (lostPlayers.includes(otherRandom)) {
+        otherRandom = randomPlayer()
+    }
+
+    lostPlayers.push(otherRandom)
+}
+
+function randomPlayer() {
+    return players[Math.floor(Math.random() * players.length)]
+}
+
+function getInfo(name) {
+    if (round >= 0) {
+        if (lostPlayers.includes(name)) {
+            return { 
+                text: "You do not know who died, make something up!",
+                image : "https://emergency.com.au/wp-content/uploads/2017/02/nervous.jpg"
+            }
+        }
+
+        return rounds[round % rounds.length]
+    }
+    
+    return { 
+        text: "The game has not started yet",
+        image: "https://clv.h-cdn.co/assets/17/24/2048x1365/funeral-etiquette-rude-behavior.jpg" 
+    }
+}
+
+const rounds = [
+    { 
+        text: "Shrek has died. Prepare a speech to honor him",
+        image: "https://www.cool-mania.se/data/product/1f/142eaf37043adae8847fa693af2bc0.png"
+    },
+    {
+        text: "Walter White has died. Prepare a speech to honor him",
+        image: "https://static1.srcdn.com/wordpress/wp-content/uploads/2020/02/Breaking-Bad-things-Walter-White-feature.jpg"
+    }
+]
